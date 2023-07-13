61517424f24d2115083231f8e6735bfd3673b971
Robert Udrea <robertudrea2002@yahoo.ro>
2023-07-13 20:07:12 +0300

    generating pawn moves

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Proiect de Licenta
 
 # Resources
 
-Pieces: https://commons.wikimedia.org/wiki/Category:PNG_chess_pieces/Standard_transparent
+Pieces: https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces#
--- main.py
+++ main.py
@@ -45,7 +45,7 @@ pieces = ['b', 'k', 'n', 'q', 'r', 'p']
 
 DARK_SQUARE_COLOR = "#7C9EB3"
 LIGHT_SQUARE_COLOR = "#D6E1E6"
-SELECTED_SQUARE_COLOR = '#8CDBEA'
+SELECTED_SQUARE_COLOR = 'green'  # '#8CDBEA'
 
 
 # 0 -> white turn
@@ -53,8 +53,19 @@ SELECTED_SQUARE_COLOR = '#8CDBEA'
 # 2 -> black turn
 # 3 -> black turn with piece selected
 turn = 0
-selected_piece = 10
+selected_piece = 99
 valid_moves = []
+captured_by_white = []
+captured_by_black = []
+
+# global white_can_passant
+white_can_passant = False
+
+# global black_can_passant
+black_can_passant = False
+
+black_pos_passant = ()
+white_pos_passant = ()
 
 
 def draw_table():
@@ -75,20 +86,130 @@ def draw_pieces():
     for i, white_piece in enumerate(white_pieces):
         existing_piece = pieces.index(white_piece)
 
+        if i == selected_piece and turn < 2:
+            pygame.draw.rect(screen, SELECTED_SQUARE_COLOR,
+                             (white_positions[i][0] * 100 + 1, white_positions[i][1] * 100 + 1, 100, 100), 5)
+
         screen.blit(white_pieces_images[existing_piece],
                     (white_positions[i][0] * 100 + PADDING, white_positions[i][1] * 100 + PADDING))
 
     for i, black_piece in enumerate(black_pieces):
         existing_piece = pieces.index(black_piece)
 
+        if i == selected_piece and turn >= 2:
+            pygame.draw.rect(screen, 'red',
+                             (black_positions[i][0] * 100 + 1, black_positions[i][1] * 100 + 1, 100, 100), 5)
+
         screen.blit(black_pieces_images[existing_piece],
                     (black_positions[i][0] * 100 + PADDING, black_positions[i][1] * 100 + PADDING))
 
-    if turn < 2:
-        i = selected_piece
-        pygame.draw.rect(screen, SELECTED_SQUARE_COLOR,
-                         (white_positions[i][0] * 100 + 1, white_positions[i][1] * 100 + 1, 100, 100), 5)
 
+# this function gets all the squares that a player can move /
+# it sums all the available moves for the remain pieces
+def get_options(pieces, positions, turn):
+    piece_moves = []
+    all_pieces_moves = []
+
+    for piece, pos in zip(pieces, positions):
+        if piece == 'p':
+            piece_moves = get_pawn_moves(pos, turn)
+        # elif piece == 'r':
+        #     piece_moves = get_rook_moves(pos, turn)
+        # elif piece == 'n':
+        #     piece_moves = get_knight_moves(pos, turn)
+        # elif piece == 'b':
+        #     piece_moves = get_bishop_moves(pos, turn)
+        # elif piece == 'q':
+        #     piece_moves = get_queen_moves(pos, turn)
+        # elif piece == 'k':
+        #     piece_moves = get_king_moves(pos, turn)
+
+        all_pieces_moves.append(piece_moves)
+    return all_pieces_moves
+
+
+def get_pawn_moves(pos, turn):
+    moves = []
+    global white_can_passant
+    global black_can_passant
+    global white_pos_passant
+    global black_pos_passant
+
+    if turn == 'white':
+        x, y = pos
+
+        if (x, y - 1) not in white_positions and (x, y - 1) not in black_positions and y - 1 >= 0:
+            moves.append((x, y - 1))
+
+        if (x, y - 2) not in white_positions and (x, y - 2) not in black_positions and y == 6:
+            moves.append((x, y - 2))
+            black_can_passant = True
+            white_pos_passant = (x, y - 2)
+
+        if (x + 1, y - 1) in black_positions:
+            moves.append((x + 1, y - 1))
+
+        if (x - 1, y - 1) in black_positions:
+            moves.append((x - 1, y - 1))
+
+        # en passant, the special move
+        # if white_can_passant:
+        #     if black_pos_passant[0] == x + 1 and black_pos_passant[1] == y:
+        #         moves.append((x + 1, y - 1))
+        #     if black_pos_passant[0] == x - 1 and black_pos_passant[1] == y:
+        #         moves.append((x - 1, y - 1))
+
+    if turn == 'black':
+        x, y = pos
+
+        if (x, y + 1) not in white_positions and (x, y + 1) not in black_positions and y + 1 < 8:
+            moves.append((x, y + 1))
+
+        if (x, y + 2) not in white_positions and (x, y + 2) not in black_positions and y == 1:
+            moves.append((x, y + 2))
+            white_can_passant = True
+            black_pos_passant = (x, y - 2)
+
+        if (x + 1, y + 1) in white_positions:
+            moves.append((x + 1, y + 1))
+
+        if (x - 1, y + 1) in white_positions:
+            moves.append((x - 1, y + 1))
+
+        # en passant, the special move
+        # if black_can_passant:
+        #     if white_pos_passant[0] == x + 1 and white_pos_passant[1] == y:
+        #         moves.append((x + 1, y + 1))
+        #     if white_pos_passant[0] == x - 1 and white_pos_passant[1] == y:
+        #         moves.append((x - 1, y + 1))
+
+    return moves
+
+
+def draw_valid_moves(moves):
+    if turn < 2:
+        color = 'green'
+    else:
+        color = 'red'
+
+    for move in moves:
+        pygame.draw.circle(
+            screen, color, (move[0]*100 + 50, move[1]*100 + 50), 5)
+
+
+# get the valid moves for the selected piece
+def get_valid_moves():
+    if turn < 2:
+        options = white_options
+    else:
+        options = black_options
+
+    valid_options = options[selected_piece]
+    return valid_options
+
+
+white_options = get_options(white_pieces, white_positions, 'white')
+black_options = get_options(black_pieces, black_positions, 'black')
 
 isRunning = True
 while isRunning:
@@ -98,10 +219,81 @@ while isRunning:
     draw_table()
     draw_pieces()
 
+    if selected_piece != 99:
+        valid_moves = get_valid_moves()
+        draw_valid_moves(valid_moves)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
 
-    pygame.display.flip()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x = event.pos[0] // 100
+            y = event.pos[1] // 100
 
+            if turn < 2:
+                if (x, y) not in white_positions and (x, y) not in valid_moves:
+                    selected_piece = 99
+
+                if (x, y) in white_positions:
+                    selected_piece = white_positions.index((x, y))
+                    turn = 1 if turn == 0 else turn
+
+                if (x, y) in valid_moves and selected_piece != 99:
+                    # if white_pieces[selected_piece] == 'p' and white_positions[selected_piece][0] == x and white_positions[selected_piece][1] - 2 == y:
+                    #     black_can_passant = True
+                    #     white_pos_passant = (x, y)
+
+                    white_positions[selected_piece] = (x, y)
+
+                    if (x, y) in black_positions:
+                        black_piece_index = black_positions.index((x, y))
+                        captured_by_white.append(
+                            black_pieces[black_piece_index])
+                        black_pieces.pop(black_piece_index)
+                        black_positions.pop(black_piece_index)
+
+                    white_options = get_options(
+                        white_pieces, white_positions, 'white')
+                    black_options = get_options(
+                        black_pieces, black_positions, 'black')
+
+                    turn = 2
+                    selected_piece = 99
+                    valid_moves = []
+                    # white_can_passant = False
+
+            if turn >= 2:
+                if (x, y) not in black_positions and (x, y) not in valid_moves:
+                    selected_piece = 99
+
+                if (x, y) in black_positions:
+                    selected_piece = black_positions.index((x, y))
+                    turn = 3 if turn == 2 else turn
+
+                if (x, y) in valid_moves and selected_piece != 99:
+                    # if black_pieces[selected_piece] == 'p' and black_positions[selected_piece][0] == x and black_positions[selected_piece][1] + 2 == y:
+                    #     white_can_passant = True
+                    #     black_pos_passant = (x, y)
+
+                    black_positions[selected_piece] = (x, y)
+
+                    if (x, y) in white_positions:
+                        white_piece_index = white_positions.index((x, y))
+                        captured_by_black.append(
+                            white_pieces[white_piece_index])
+                        white_pieces.pop(white_piece_index)
+                        white_positions.pop(white_piece_index)
+
+                    white_options = get_options(
+                        white_pieces, white_positions, 'white')
+                    black_options = get_options(
+                        black_pieces, black_positions, 'black')
+
+                    turn = 0
+                    selected_piece = 99
+                    valid_moves = []
+                    # black_can_passant = False
+
+    pygame.display.flip()
 pygame.quit()
