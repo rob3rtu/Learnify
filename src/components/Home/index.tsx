0a07f51f8a975fc335542dc2ecc03859eca4e667
Robert Udrea <robert.udrea@cst.ro>
2023-11-05 16:49:16 +0200

    setup filters

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,16 @@
-import { Flex, SimpleGrid } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Store";
 import { colors } from "../../theme";
 import { NavBar } from "./NavBar";
-import { CourseInterface } from "./types";
+import { CourseInterface, FiltersInterface, defaultFilters } from "./types";
 import { CourseCard } from "./CourseCard";
+import { useState } from "react";
 
 export const Home = () => {
   const account = useSelector((state: RootState) => state.auth.account);
+  const [filters, setFilters] = useState<FiltersInterface>(defaultFilters);
+
   const temporaryCourses: CourseInterface[] = [
     {
       shortName: "PAO",
@@ -41,16 +44,27 @@ export const Home = () => {
     >
       <NavBar user={account} />
 
-      <SimpleGrid
-        width="100vw"
-        padding={20}
-        spacing={20}
-        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-      >
-        {temporaryCourses.map((course, i) => (
-          <CourseCard key={i} course={course} />
-        ))}
-      </SimpleGrid>
+      {temporaryCourses.length ? (
+        <SimpleGrid
+          width="100vw"
+          padding={20}
+          spacing={20}
+          templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+        >
+          {temporaryCourses.map((course, i) => (
+            <CourseCard key={i} course={course} />
+          ))}
+        </SimpleGrid>
+      ) : (
+        <Text
+          marginTop={20}
+          fontFamily="WorkSans-SemiBold"
+          fontSize={30}
+          color={colors.white}
+        >
+          There is no course available.
+        </Text>
+      )}
     </Flex>
   );
 };
--- src/components/Home/types.ts
+++ src/components/Home/types.ts
@@ -2,3 +2,15 @@ export interface CourseInterface {
   shortName: string;
   longName: string;
 }
+
+export interface FiltersInterface {
+  domain: "mate" | "info" | "cti";
+  year: 1 | 2 | 3 | 4;
+  semester: 1 | 2;
+}
+
+export const defaultFilters: FiltersInterface = {
+  domain: "mate",
+  year: 1,
+  semester: 1,
+};
