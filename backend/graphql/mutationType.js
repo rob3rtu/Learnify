9a5558aac1c607f7da8b399b5ac0f9f95617cf03
Robert Udrea <robert.udrea@cst.ro>
2024-01-28 14:04:48 +0200

    delete course endpoint

--- backend/graphql/mutationType.js
+++ backend/graphql/mutationType.js
@@ -2,6 +2,7 @@ const { GraphQLObjectType } = require("graphql");
 const login = require("./mutations/login");
 const saveProfileImage = require("./mutations/saveProfileImage");
 const addCourse = require("./mutations/addCourse");
+const deleteCourse = require("./mutations/deleteCourse");
 
 const mutationType = new GraphQLObjectType({
   name: "Mutation",
@@ -9,6 +10,7 @@ const mutationType = new GraphQLObjectType({
     login: login,
     saveProfileImage: saveProfileImage,
     addCourse: addCourse,
+    deleteCourse: deleteCourse,
   },
 });
 
