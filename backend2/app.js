2c59058b502908286f4139a7bb0492be4d14ace4
Robert Udrea <robert.udrea@cst.ro>
2024-02-04 16:50:02 +0200

    new backend express setup

---
+++ backend2/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
