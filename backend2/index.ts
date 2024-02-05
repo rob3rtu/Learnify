aa6201a28f94a0ad34720c6a31080786c0297425
Robert Udrea <robert.udrea@cst.ro>
2024-02-05 22:26:22 +0200

    login endpoint remake

--- backend2/index.ts
+++ backend2/index.ts
@@ -1,19 +1,35 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors = require("cors");
+import "dotenv/config";
+import authRouter from "./routes/auth";
+import { PrismaClient } from "@prisma/client";
+
+require("dotenv").config();
 
 const prisma = new PrismaClient();
 
 const port = 3001;
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
+  res.send("Hello there");
 });
 
-app.listen(port, () => {
+app.use("/api/auth", authRouter);
+
+const checkDbConnection = async () => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    console.log("Database connected");
+  } catch (error) {
+    console.log("Database missing");
+  }
+};
+
+app.listen(port, async () => {
   console.log(`App running on port ${port}`);
+  await checkDbConnection();
 });
---
+++ backend2/routes/auth.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import { PrismaClient, enum_Users_role } from "@prisma/client";
+import sgMail from "@sendgrid/mail";
+import { sign } from "jsonwebtoken";
+
+const authRouter = express.Router();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY ?? "");
+const prisma = new PrismaClient();
+
+authRouter.post("/login", async (req, res) => {
+  const email = req.body.email;
+
+  let user = await prisma.user.findFirst({ where: { email } });
+
+  //if there is no user, create one with this email
+  //maybe change this behaviour in the future
+  if (user === null) {
+    user = await prisma.user.create({
+      data: {
+        email,
+        fullName: email.split("@")[0],
+        role: enum_Users_role.student,
+      },
+    });
+  }
+
+  try {
+    const token = sign(
+      {
+        id: user.id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+        profileImage: user.profileImage,
+      },
+      process.env.JWT_KEY ?? "",
+      { expiresIn: "1h" }
+    );
+
+    await sgMail.send({
+      to: email,
+      from: process.env.SENDGRID_EMAIL ?? "",
+      subject: "Log in to LERNIFY",
+      html: `<a href='http://localhost:3000/confirm-email?token=${token}'>Log in</a>`,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Email not sent." });
+  }
+
+  res.sendStatus(200);
+});
+
+export default authRouter;
